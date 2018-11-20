377cee1a9022a44977e355f387f7b76290771e2f
wxl <wxl200854@126.com>
2018-11-20 12:56:06 +0800

    20181120-12:56

--- pachong/spider.py
+++ pachong/spider.py
@@ -4,24 +4,29 @@ from urllib.parse import urlencode
 
 def get_page_index(offset, keyword):
     data={
-        'offset': offset
-        'format': 'json'
-        'keyword': keyword
-        'autoload': 'true'
-        'count': '20'
-        'cur_tab': 1
+        'offset': offset,
+        'format': 'json',
+        'keyword': keyword,
+        'autoload': 'true',
+        'count': '20',
+        'cur_tab': '1',
+        'from': 'search_tab'
     }
-    url = 'https://www.toutiao.com/search_content/?' + uelencode(data)
-   try:
+    url = 'https://www.toutiao.com/search_content/?' + urlencode(data)
+    try:
         response = requests.get(url)
         if response.status_code == 200:
             return response.text
         return None
     except RequestException:
+        print("请求索引失败")
         return None
 
+def parse_page_index(html):
+    
+
 def main():
-    html = get_page_index(0, '街拍')
+    html = get_page_index(0, "街拍")
     print(html)
 
 if __name__ == "__main__":
