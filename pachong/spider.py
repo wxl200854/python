7d563888bfef4eba37c7963303a8e18111f0c7a2
wxl <wxl200854@126.com>
2018-12-05 14:06:15 +0800

    spider.py 20181205 14:06

--- pachong/spider.py
+++ pachong/spider.py
@@ -4,7 +4,13 @@ from urllib.parse import urlencode
 import json
 import re 
 from bs4 import BeautifulSoup
+import codecs
+from config import *
+import pymongo
 
+client = pymongo.MongoClient(MONGO_URL)
+db = client[MONGO_DB]
+    
 def get_page_index(offset, keyword):
     data={
         'offset': offset,
@@ -40,7 +46,8 @@ def get_page_detail(url):
         'cache-control': 'max-age=0',
         'upgrade-insecure-requests': '1',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36',
-        'referer':'https://www.toutiao.com/a6602192672943768067/'
+        #'referer':'https://www.toutiao.com/a6602192672943768067/'
+        'referer': url
     }
     try:
         response = requests.get(url, headers=headers)
@@ -51,21 +58,41 @@ def get_page_detail(url):
         print('请求索引出错', url)
         return None
 
-def parse_page_detail(html):
+def parse_page_detail(html, url):
     soup = BeautifulSoup(html, 'lxml')
     title = soup.select('title')[0].get_text()
     print(title)
     image_pattern = re.compile('gallery: JSON\.parse\("(.*?)"\),', re.S)
     result = re.search(image_pattern, html)
     if result:
-        print(result.group(1))
+        data_str = codecs.getdecoder('unicode_escape')(result.group(1))[0]
+        json_data = json.loads(data_str)
+        if json_data and 'sub_images' in json_data.keys():
+            sub_images = json_data.get('sub_images')
+            images = [item.get('url') for item in sub_images]
+            #data = json.loads(codecs.getdecoder('unicode_escape')(result.group(1)))        
+            #if data and 'sub_images' in data.keys():
+            # sub_images = data.get('sub_images')
+            #images = [item.get('url') for item in sub_images]
+            return {
+                'title': title,
+                'url': url,
+                'images': images
+            }
+
+def save_to_mongo(result):
+    if db[MONGO_TABLE].insert(result):
+        print("存储到mongodb成功", result)
+        return True
+    return False            
 
 def main():
     html = get_page_index(0, '街拍')
     for url in parse_page_index(html):
         html = get_page_detail(url)
         if html:
-            parse_page_detail(html)
+            result = parse_page_detail(html, url)
+            save_to_mongo(result)
 
 if __name__ == "__main__":
     main()
