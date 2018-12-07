445f9c57a71cf44dd789df710e5f70ebd4163f33
wxl <wxl200854@126.com>
2018-12-07 15:09:13 +0800

    123

---
+++ pachong/btbtdy/spider.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+from requests.exceptions import RequestException
+import re 
+import json
+
+def get_index(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+             return response
+        return None
+    except RequestException:
+        return None
+    
+
+def parse_index(response):
+    soup = BeautifulSoup(response.text, 'lxml')
+    html = soup.select('.new-ul')
+    print(html)
+
+def main():
+    url = 'http://www.btbtdy.net/hot/'
+    response = get_index(url)
+    parse_index(response)
+    
+
+
+if __name__ == "__main__":
+    main()
