dd2c1045186b6d3ce4fc2a1cca744856786ae52b
wxl's rust <wxl200854@126.com>
2019-03-03 02:30:15 +0800

    123'

--- test/test01.py
+++ test/test01.py
@@ -1,3 +1,26 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-print("Hello, world!")
+import random
+
+def getAnswer(answerNumber):
+    if answerNumber == 1:
+        return 'It is certain'
+    elif answerNumber == 2:
+        return 'It is decidedly so'
+    elif answerNumber == 3:
+        return 'Yes'
+    elif answerNumber == 4:
+        return 'Reply hazy try again'
+    elif answerNumber == 5:
+        return 'Ask again later'
+    elif answerNumber == 6:
+        return 'Concentrate and ask again'
+    elif answerNumber == 7:
+        return 'My reply is no'
+    elif answerNumber == 8:
+        return 'Outlook not so good'
+    elif answerNumber == 9:
+        return 'Very doubtful'
+
+r = random.randint(1, 9)
+print(r)
+fortune = getAnswer(r)
+print(fortune)
