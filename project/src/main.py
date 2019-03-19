f16daaf5bc339645da2dc2bee487bcca4c686e93
wxl's rust <wxl200854@126.com>
2019-03-20 00:51:32 +0800

    book 2019-3-20 0:51

--- project/src/main.py
+++ project/src/main.py
@@ -10,42 +10,54 @@ from selenium.webdriver.support.wait import WebDriverWait
 import time
 from pyquery import PyQuery as pq
 
-browser = webdriver.Chrome()
-def get_all_url():
-    try:
-        browser.get(URL)
-        input = True
-        while(input):
-            input = browser.find_element_by_class_name("next")
-            get_page_url()
+browser = webdriver.PhantomJS()
 
-            input.click()
-            time.sleep(1)
-    finally:
-        browser.close()
+def get_all_url():
+        try:
+                browser.get(URL)
+                input = True
+                while(input):
+                input = browser.find_element_by_class_name("next")
+                get_page_detail()
+                input.click()
+                time.sleep(1)
+        finally:
+                browser.close()
 
 def get_page_detail():
-    html = browser.page_source
-    doc = pq(html)
-    items = doc('.selected .entry .item').items()
-    for item in items:
-        name = item.find('.warp .t').text()
-        intro = item.find('.warp .p').text()
-        all = item.find('.warp .intro').text().replace('\n', '').split('|')
-        author = all[0]
-        style = all[1]
-        status = all[2]       
-        img = item.find('.avatar a img').attr("src")
-        url = item.find('.avatar a').attr('href')
-        print(author, style, status)
+        html = browser.page_source
+        doc = pq(html)
+        items = doc('.selected .entry .item').items()
+        for item in items:
+                name = item.find('.warp .t').text()
+                intro = item.find('.warp .p').text()
+                all = item.find('.warp .intro').text().replace('\n', '').split('|')
+                author = all[0] 
+                style = all[1]
+                status = all[2]       
+                img = item.find('.avatar a img').attr("src")
+                url = item.find('.avatar a').attr('href')
+                # print(name, author, style, status, intro, url, img)
+
+def get_book_list(url):
+        headers = {
+                'User-Agent': UserAgent().random(),
+                'Cookie': 'PHPSESSID=4lj9a88236t7s44bas23str817; qrxs_auth=ee12%2FDwoP5R4P2Dcm1hjjrCeOoPYU%2FpRLbhmzFrnzL9M8XzUvmlMyZV%2Bxj6I9yBIk0ziKuDAg5eIgEmM1E9jWPynug; Hm_lvt_9ed2be7ae49dbc8829f60b2d671b5654=1553007861,1553009454; SERVERID=f4ac482318e2356f12270179e2238604|1553009764|1553007858; Hm_lpvt_9ed2be7ae49dbc8829f60b2d671b5654=1553009765'
+        }
+        url_booklist = URL_BASE + url + "list"
+        resquest = requests.get(url_booklist, headers = headers)
+        doc = pq(requests.text)
+        items = doc('.main.all_chapter.mar_top .item .ul .li').items()
+        for item in items:
+                name = item.find('a span').text()
+                url_chapter = item.find('a').attr('href')
 
 
-     
+
+
 
 
 
 if __name__ == "__main__":
     get_all_url()
-
-
 
---
+++ project/src/test.py
@@ -0,0 +1,22 @@
+from fake_useragent import UserAgent
+import requests
+from pyquery import PyQuery as pq
+
+def get_book_list():
+        headers = {
+                'User-Agent': UserAgent().random,
+                'Cookie': 'PHPSESSID=4lj9a88236t7s44bas23str817; qrxs_auth=ee12%2FDwoP5R4P2Dcm1hjjrCeOoPYU%2FpRLbhmzFrnzL9M8XzUvmlMyZV%2Bxj6I9yBIk0ziKuDAg5eIgEmM1E9jWPynug; Hm_lvt_9ed2be7ae49dbc8829f60b2d671b5654=1553007861,1553009454; SERVERID=f4ac482318e2356f12270179e2238604|1553009764|1553007858; Hm_lpvt_9ed2be7ae49dbc8829f60b2d671b5654=1553009765'
+        }
+        # url_booklist = 'https://www.qirexiaoshuo.com/book/41915/list'
+        url_booklist = 'https://www.qirexiaoshuo.com/book/13023/list/'
+        res = requests.get(url_booklist, headers = headers)
+        doc = pq(res.text)
+        items = doc('.main.all_chapter.mar_top .item ul li').items()
+        for item in items:
+                name = item.find('a span').text()
+                url_chapter = item.find('a').attr('href')
+                print(name, url_chapter)
+
+
+if __name__ == "__main__":
+    get_book_list()
